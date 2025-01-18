9e7c44ba65446ca2130933e459202344f48ef1b5
tamiopia <tamiopia23@gmail.com>
2025-01-18 11:45:54 +0300

    optimazation done on the compresd file txt

--- text_decode.cpp
+++ text_decode.cpp
@@ -9,182 +9,162 @@
 #include "encode.h"
 #define _CRT_SECURE_NO_WARNINGS
 #include <stack>
-#include <sys/stat.h>  // For mkdir on POSIX systems
-#include <direct.h>    // For mkdir on Windows
-
 using namespace std;
 
-struct Tree_node {
-    char data;
-    Tree_node* left;
-    Tree_node* right;
-    Tree_node(char data) {
-        this->left = NULL;
-        this->right = NULL;
-        this->data = data;
-    }
+struct Tree_node 
+//tree node for rebuilding huffman tree
+{
+	char data;
+	Tree_node* left;
+	Tree_node* right;
+	Tree_node(char data) {
+		this->left = NULL;
+		this->right = NULL;
+		this->data = data;
+	}
+
 };
 
-int create_folder(const char* folder_path) {
-    // Attempt to create the directory (Windows or POSIX systems)
-    #ifdef _WIN32
-        if (_mkdir(folder_path) == 0) {
-            cout << "Folder created: " << folder_path << endl;
-            return 0;
-        } else {
-            cout << "folder already exists: " << folder_path << endl;
-            return 0;  // Changed to -1 for failure
-        }
-    #else
-        if (mkdir(folder_path, 0777) == 0) {
-            cout << "Folder created: " << folder_path << endl;
-            return 0;
-        } else {
-            cout << "Failed to create folder or folder already exists: " << folder_path << endl;
-            return -1;  // Changed to -1 for failure
-        }
-    #endif
+unsigned int read_header(FILE* fr) {
+	// helper function to read first three unsigned int
+	unsigned int buffer;
+	fread(&buffer,sizeof(buffer),1,fr);
+	return buffer;
 }
 
-unsigned int read_header(FILE* fr) {
-    unsigned int buffer;
-    fread(&buffer, sizeof(buffer), 1, fr);
-    return buffer;
-}
 
 Tree_node* rebuild_huffman_tree(FILE* fr) {
-    struct Tree_node* left, * right, * top;
-    stack<Tree_node*> stack;
-    unsigned int header_len = read_header(fr);
-    unsigned int map_len = read_header(fr);
-    cout << "The header has total length of " << header_len << endl;
-    int idx_header = 0;
-    unsigned char str;
-    unsigned int num;
-    string tmp = "";
-    string bin_num;
-    bool flag = true;
-    while (idx_header < header_len) {
-        fread(&str, sizeof(str), 1, fr);
-        num = int(str);
-        bin_num = int_to_binary(num);
-        int idx_loop = 0;
-        while (idx_loop < bin_num.size()) {
-            if (tmp.size() == 0 && flag == true) {
-                if (bin_num[idx_loop] == '1') {
-                    flag = false;
-                } else {
-                    if (stack.size() == 1) {
-                        break;
-                    }
-                    Tree_node* right_node = stack.top();
-                    stack.pop();
-                    Tree_node* left_node = stack.top();
-                    stack.pop();
-                    Tree_node* new_node = new Tree_node('#');
-                    new_node->left = left_node;
-                    new_node->right = right_node;
-                    stack.push(new_node);
-                    if (map_len == 0) {
-                        flag = true;
-                    }
-                }
-            } else {
-                tmp += bin_num[idx_loop];
-            }
-            if (tmp.size() == 8) {
-                char new_char = binary_to_char(tmp);
-                Tree_node* new_node = new Tree_node(new_char);
-                stack.push(new_node);
-                tmp = "";
-                map_len -= 1;
-                flag = true;
-            }
-            idx_loop += 1;
-        }
-        idx_header += 1;
-    }
-    return stack.top();
+	// we will use a stack to rebuild our huffman tree
+	// when we see a "1", it represents a leaf node, 
+	// we will create a new node and push to the stack with following char value
+	// " 0" denotes the non-leaf node, we will pop the first two nodes and merge them
+	struct Tree_node* left, * right,* top;
+	stack<Tree_node*> stack;
+	unsigned int header_len= read_header(fr);
+	unsigned int map_len= read_header(fr);
+	cout << " the header has total length of "<<header_len<<endl;
+	int idx_header = 0;
+	unsigned char str;
+	unsigned int num;
+	string tmp = "";
+	string bin_num;
+	bool flag = true;
+	while (idx_header<header_len){
+		fread(&str,sizeof(str),1,fr);
+		num = int(str);
+		bin_num = int_to_binary(num);
+		// cout << " now read binary "<<bin_num<<endl;
+		int idx_loop = 0;
+		while (idx_loop < bin_num.size()){
+			if (tmp.size() == 0 and flag == true){
+				if (bin_num[idx_loop] == '1'){
+					flag = false;
+				}
+				else{
+					// current is 0 which is  a non_leaf node
+					if (stack.size()==1){
+						break;
+					}
+					Tree_node* right_node = stack.top();
+					stack.pop();
+					Tree_node* left_node = stack.top();
+					stack.pop();
+					Tree_node* new_node = new Tree_node('#');
+					new_node->left = left_node;
+					new_node->right = right_node;
+					stack.push(new_node);
+					if (map_len == 0){
+						flag = true;
+					}
+					}
+					
+			}
+			else{
+				tmp += bin_num[idx_loop];}
+			if (tmp.size() == 8){
+				char new_char = binary_to_char(tmp);
+				// cout << " now push new tree node "<<new_char<<endl;
+				Tree_node* new_node = new Tree_node(new_char);
+				stack.push(new_node);
+				tmp = "";
+				map_len -= 1;
+				flag = true;
+			}
+			idx_loop += 1;
+		}
+		idx_header += 1;
+	}
+	return stack.top();
 }
 
-unordered_map<string, char> build_decoding_map(Tree_node* root, string str, unordered_map<string, char> map, unsigned int file_len) {
-    if (!root) {
-        return map;
-    }
-    if (root->data != '#') {
-        map[str] = root->data;
-    }
-    map = build_decoding_map(root->left, str + "0", map, file_len);
-    map = build_decoding_map(root->right, str + "1", map, file_len);
-    return map;
+unordered_map<string,char> build_decoding_map(Tree_node* root,string str,unordered_map<string, char> map,unsigned int file_len){
+	// tranverse our huffman tree in post order to build our decoding map 
+	if (!root) {
+		return map;
+	}
+	if (root->data != '#') {
+		map[str] = root->data;
+	}
+	map = build_decoding_map(root->left, str + "0",map,file_len);
+	map = build_decoding_map(root->right, str + "1",map,file_len);
+	return map;
 }
 
-void decoding(FILE* fr, FILE* fw, unordered_map<string, char> map, int file_len) {
-    string tmp = "";
-    int idx;
-    unsigned char str;
-    int num;
-    string bin_num;
-    int current_length = 0;
-    while (!feof(fr)) {
-        idx = 0;
-        fread(&str, sizeof(str), 1, fr);
-        num = int(str);
-        bin_num = int_to_binary(num);
-        while (idx < bin_num.size()) {
-            if (current_length == file_len) {
-                break;
-            }
-            tmp += bin_num[idx];
-            idx += 1;
-            if (map.find(tmp) != map.end()) {
-                unsigned char buff = map[tmp];
-                fwrite(&buff, sizeof(buff), 1, fw);
-                cout << "Written byte: " << buff << endl;  // Debugging line
-                tmp = "";
-                current_length += 1;
-            }
-        }
-    }
-    cout << "Decoding successfully!" << endl;
+void decoding(FILE* fr, FILE* fw,unordered_map<string, char> map,int file_len) {
+	// read a single bit at a time and use our previously built huffman map to encode
+	string tmp = "";
+	int idx;
+	unsigned char str;
+	int num;
+	string bin_num;
+	int current_length = 0;
+	while (!feof(fr)){
+		idx = 0;
+		fread(&str,sizeof(str),1,fr);
+		num = int(str);
+		bin_num = int_to_binary(num);
+		while (idx < bin_num.size()){
+			if (current_length == file_len){
+				break;
+			}
+			tmp += bin_num[idx];
+			idx += 1;
+			if (map.find(tmp) != map.end()){
+				unsigned char buff = map[tmp];
+				// cout << "now write char "<<buff<<endl;
+				fwrite(&buff,sizeof(buff),1,fw);
+				tmp = "";
+				current_length += 1;
+			}
+		}
+		
+	}
+	cout <<"decoding successfully!"<<endl;
 }
+
+
 
 int main() {
-    const char* decoded_folder = "decoded_files";
-    int folder_result = create_folder(decoded_folder);
-    if (folder_result == -1) {
-        cerr << "Error creating folder. Exiting program." << endl;
-        return 0;
+	// open the input and output file
+	const char* input_file = "test_output_encoded.txt";
+	const char* output_file= "test_output_decoded.txt";
+	FILE* input = fopen(input_file, "rb");
+	int result = open_file(input);
+	if (result == -1){
+		return 0;
     }
-
-    const char* input_file = "encoded_files/test_output_encoded.txt";
-    const char* output_file = "decoded_files/test_output_decoded.txt";
-
-    FILE* input = fopen(input_file, "rb");
-    if (!input) {
-        cerr << "Failed to open input file: " << input_file << endl;
-        return 0;
+	unsigned int file_len = read_header(input);
+	Tree_node* top;
+	top = rebuild_huffman_tree(input);
+	unordered_map<string, char> decoding_map;
+	decoding_map = build_decoding_map(top,"",decoding_map,file_len);
+	FILE* output = fopen(output_file, "w+");
+	result = open_file(output);
+	if (result == -1){
+		return 0;
     }
-
-    unsigned int file_len = read_header(input);
-    Tree_node* top = rebuild_huffman_tree(input);
-    unordered_map<string, char> decoding_map;
-    decoding_map = build_decoding_map(top, "", decoding_map, file_len);
-
-    FILE* output = fopen(output_file, "wb");  // Open in binary mode for writing
-    if (!output) {
-        cerr << "Failed to open output file: " << output_file << endl;
-        return 0;
-    } else {
-        cout << "Output file opened successfully: " << output_file << endl;
-    }
-
-    decoding(input, output, decoding_map, file_len);
-
-    fclose(input);
-    fclose(output);
-
-    cout << "Decoding completed successfully!" << endl;
-
-    return 0;
+	decoding(input,output,decoding_map,file_len);
+	fclose(input);
+	fclose(output);
+	return 0;
 }
--- text_encode.cpp
+++ text_encode.cpp
@@ -5,170 +5,327 @@
 #include <string>
 #include <queue>
 #include <fstream>
-#include <string>
-#include "encode.h"
 #define _CRT_SECURE_NO_WARNINGS
-#include <stack>
-#include <sys/stat.h>  // For mkdir on POSIX systems
-#include <direct.h>    // For mkdir on Windows
-
 using namespace std;
 
-struct Tree_node {
-    // tree node for building huffman tree
-    char data;
-    Tree_node* left;
-    Tree_node* right;
-    Tree_node(char data) {
-        this->left = NULL;
-        this->right = NULL;
-        this->data = data;
-    }
+struct minheapnode 
+//minheap node for building huffman heap
+{
+	unsigned freq;
+	char data;
+	minheapnode* left;
+	minheapnode* right;
+	minheapnode(char data, int freq) {
+		this->left = NULL;
+		this->right = NULL;
+		this->data = data;
+		this->freq = freq;
+	}
+
+};
+struct compare {
+	bool operator()(minheapnode* a, minheapnode* b) {
+		return a->freq > b->freq;
+	}
 };
 
-int create_folder(const char* folder_path) {
-    // Attempt to create the directory (Windows or POSIX systems)
-    #ifdef _WIN32
-        // On Windows, use _mkdir
-        if (_mkdir(folder_path) == 0) {
-            cout << "Folder created: " << folder_path << endl;
-            return 0;
-        } else {
-            cout << " folder already exists: " << folder_path << endl;
-            return 0;
+unordered_map<char, int> build_frequency_table(FILE* input) {
+    // count word frequency from file, and output as a hashmap
+	unordered_map<char, int> map;
+	char tmp;
+	while (true) {
+		tmp = fgetc(input);
+		if (tmp == EOF) {
+			cout << "frequency map build successfully!"<<endl;
+			break;
+		}
+		if (map.find(tmp) != map.end()) {
+			map[tmp] += 1;
+		}
+		else {
+			map[tmp] = 1;
+		}
+	}
+	return map;
+}
+
+
+priority_queue<minheapnode*, vector<minheapnode*>, compare> build_huffman_heap(unordered_map<char, int> map) {
+    // build huffman heap by using a min heap
+	struct minheapnode* left, * right, * top;
+	priority_queue<minheapnode*, vector<minheapnode*>, compare> minheap;
+	unordered_map<char, int>::iterator itr;
+	for (itr = map.begin(); itr != map.end();itr++) {
+		char key = itr->first;
+		int val = itr->second;
+		minheapnode* node = new minheapnode(key, val);
+		minheap.push(node);
+	}
+	while (minheap.size() != 1) {
+		minheapnode* node1 = minheap.top();
+		minheap.pop();
+		minheapnode* node2 = minheap.top();
+		minheap.pop();
+		minheapnode* new_node = new minheapnode('#', node1->freq + node2->freq);
+		new_node->left = node1;
+		new_node->right = node2;
+		minheap.push(new_node);
+	}
+	return minheap;
+
+	
+}
+
+int get_header_length(int len, minheapnode* root) {
+    // we could build a header to store our huffman tree
+	if (!root) {
+		return 0;
+	}
+	int left, right, current;
+	left = get_header_length(0, root->left);
+	right = get_header_length(0, root->right);
+	if (root->data != '#') {
+		current = 1;
+	}
+	else {
+		current = 0;
+	}
+	return left + right + current;
+}
+
+void print_huffman_tree(minheapnode*root,string str) {
+    // use for debugging and print the huffman tree in order
+	if (!root) {
+		return;
+	}
+	if (root->data != '#') {
+		cout << " the string " <<" "<< root->data << " with frequency " << root->freq << " has huffman code " << str << endl;
+	}
+	print_huffman_tree(root->left, str + "0");
+	print_huffman_tree(root->right, str + '1');
+}
+
+unordered_map<char, string> build_encoding_map(minheapnode* root, string str, unordered_map<char, string> encoding_map) {
+    // build hash map
+	if (!root) {
+		return encoding_map;
+	}
+	if (root->data != '#') {
+		encoding_map[root->data] = str;
+	}
+	encoding_map = build_encoding_map(root->left, str + "0",encoding_map);
+	encoding_map = build_encoding_map(root->right, str + "1",encoding_map);
+	return encoding_map;
+}
+
+
+string compress_huffman_tree(string str,minheapnode* root) {
+    // compress the huffman tree by post order so that we can store it in our header
+	string left = "";
+	string right = "";
+	string current = "";
+	if (!root) {
+		return "";
+	}
+	left = compress_huffman_tree(str, root->left);
+	right = compress_huffman_tree(str, root->right);
+	if (root->data != '#') {
+		current = "1";
+		current += root->data;
+	}
+	else {
+		current += "0";
+	}
+    // cout << str + left + right + current;
+    // cout<<"---"<<endl;
+	return str + left + right + current;
+
+}
+
+string int_to_binary(int num) {
+    // helper function to tranform integer to binary number in string format
+	string str = "00000000";
+	int idx = 0;
+	while (num > 0) {
+		if (num % 2 != 0) {
+			str[idx] = '1';
+		}
+		idx++;
+		num = num / 2;
+	}
+	return str;
+
+
+}
+unsigned char binary_to_char(string str) {
+    // helper function to transform binary number to char by using ascii code
+	int tmp = 0;
+	for (int i = 0; i < 8;i++) {
+		if (str[i] == '1') {
+			tmp += pow(2, i);
+		}
+	}
+	unsigned char ans = char(tmp);
+	return ans;
+}
+
+
+void write_header(FILE* fw,string str) {
+    // write the compressed huffman tree to our file
+	string tmp = "00000000";
+	int idx = 0;
+	unsigned char buff;
+	int ascii_value;
+    	bool flag = true;
+	string bin;
+	for (int i = 0;i < str.size();i++) {
+        if (flag == true) {
+            tmp[idx] = str[i];
+            if (tmp[idx] == '1'){
+                flag = false;
+            }
+            idx+=1;
         }
-    #else
-        // On POSIX (Linux/Mac), use mkdir
-        if (mkdir(folder_path, 0777) == 0) {
-            cout << "Folder created: " << folder_path << endl;
-            return 0;
-        } else {
-            cout << "Failed to create folder or folder already exists: " << folder_path << endl;
-            return -1;
+        else {
+            ascii_value = int(str[i]);
+            bin = int_to_binary(ascii_value);
+            for (int j = 0; j < tmp.size();j++) {
+                tmp[idx] = bin[j];
+                idx+=1;
+                if (idx == 8) {
+                    idx = 0;
+                    buff = binary_to_char(tmp);
+                    // cout << "now write binary number to file "<< tmp << endl;
+                    tmp = "00000000";
+                    fwrite(&buff, sizeof(unsigned char), 1, fw);
+                }
+            }
+            flag = true;
+
         }
-    #endif
+
+        if (idx == 8) {
+            idx = 0;
+            buff = binary_to_char(tmp);
+            fwrite(&buff, sizeof(unsigned char), 1, fw);
+            // cout << "now write binary number to file "<< tmp << endl;
+            tmp = "00000000";
+            }
+
+	}
+	if (idx != 0) {
+		for (int pos = idx;pos < tmp.size();pos++) {
+			tmp[pos] = '0';
+		}
+		buff = binary_to_char(tmp);
+        // cout << "now write binary number to file "<< tmp << endl;
+		fwrite(&buff, sizeof(unsigned char), 1, fw);
+	}
+	return;
+    }
+
+int open_file(FILE* input){
+	if (input == NULL) {
+		cout << "File did not open successfully, please check " << endl;
+		return -1;
+	}
+	else {
+		cout << "File opened successfully!" << endl;
+        return 0;
+	}
 }
 
-unordered_map<char, int> build_frequency_table(FILE* fr) {
-    unordered_map<char, int> freq_table;
-    unsigned char ch;
-    while (fread(&ch, sizeof(ch), 1, fr)) {
-        freq_table[ch]++;
-    }
-    return freq_table;
+int count_length(FILE* input){
+    	// count the length of input file
+    	fseek(input, 0L, SEEK_END);
+	unsigned int file_len = ftell(input);
+	if (fseek(input, 0, SEEK_SET) != 0) {
+		cout << "failed to rewind the pointer for input file" << endl;
+	return -1;
+	}
+	else{
+	cout << "the input file has total " << file_len << " bytes" << endl;
+	return file_len;
+	}
 }
 
-struct Compare {
-    bool operator()(Tree_node* left, Tree_node* right) {
-        return left->data > right->data;  // Min heap
-    }
-};
 
-Tree_node* build_huffman_tree(unordered_map<char, int> freq_table) {
-    priority_queue<Tree_node*, vector<Tree_node*>, Compare> min_heap;
-    
-    // Create leaf nodes and add them to the priority queue
-    for (const auto& pair : freq_table) {
-        min_heap.push(new Tree_node(pair.first));
-    }
 
-    // Build the Huffman Tree
-    while (min_heap.size() > 1) {
-        Tree_node* left = min_heap.top(); min_heap.pop();
-        Tree_node* right = min_heap.top(); min_heap.pop();
+void encoding(FILE* fr,FILE*fw, unordered_map<char, string>map) {
+    // use previous built huffman tree to encode our input file
+	char str;
+	int idx = 0;
+	string tmp = "00000000";
+    	fseek(fr, 0, SEEK_SET); //set the pointer to the beginning of our input file
+	unsigned char buff;
+	string huff_code;
+	while (1) {
+		str = fgetc(fr);
+		if (str == EOF) {
+			if (idx != 0) {
+				for (int i = idx; i < 8; i++) {
+					tmp[i] = 0;
+				}
+				buff = binary_to_char(tmp);
+				fwrite(&buff, sizeof(unsigned char), 1, fw);
+			}
+			break;
+		}
+		huff_code = map[str];
+        // cout << "encoded string "<<str<<endl;
+		for (int i = 0;i < huff_code.size();i++) {
+			tmp[idx] = huff_code[i];
+			idx += 1;
+			if (idx == 8) {
+				idx = 0;
+				buff = binary_to_char(tmp);
+				fwrite(&buff, sizeof(unsigned char), 1, fw);
+				// cout << "write successfully";
+				tmp = "00000000";
+			}
+		}
+		
+	}
+	cout << "file compressed successfully" << endl;
 
-        Tree_node* new_node = new Tree_node('#');
-        new_node->left = left;
-        new_node->right = right;
-        min_heap.push(new_node);
-    }
-
-    return min_heap.top();  // Return root of the Huffman tree
 }
-
-void build_encoding_map(Tree_node* root, string str, unordered_map<char, string>& encoding_map) {
-    if (!root) return;
-
-    // If it's a leaf node, store the encoding
-    if (root->data != '#') {
-        encoding_map[root->data] = str;
-    }
-
-    build_encoding_map(root->left, str + "0", encoding_map);
-    build_encoding_map(root->right, str + "1", encoding_map);
-}
-
-void encode(FILE* fr, FILE* fw, unordered_map<char, string> encoding_map) {
-    unsigned char ch;
-    string encoded_str;
-    while (fread(&ch, sizeof(ch), 1, fr)) {
-        encoded_str += encoding_map[ch];
-    }
-
-    // Write the encoded string to the output file
-    while (encoded_str.size() >= 8) {
-        string byte_str = encoded_str.substr(0, 8);
-        unsigned char byte = binary_to_char(byte_str);
-        fwrite(&byte, sizeof(byte), 1, fw);
-        encoded_str = encoded_str.substr(8);
-    }
-
-    // Handle remaining bits (less than 8)
-    if (encoded_str.size() > 0) {
-        encoded_str = encoded_str + string(8 - encoded_str.size(), '0');
-        unsigned char byte = binary_to_char(encoded_str);
-        fwrite(&byte, sizeof(byte), 1, fw);
-    }
-
-    cout << "Encoding successfully!" << endl;
-}
-
 int main() {
-    // Create the encoded_files folder if it doesn't exist
-    const char* encoded_folder = "encoded_files";
-    int folder_result = create_folder(encoded_folder);
-    if (folder_result == -1) {
-        return 0;  // Exit if the folder couldn't be created or already exists
-    }
-
-    // Define the input and output file paths
-    const char* input_file = "test.txt";
-    const char* output_file = "encoded_files/test_output_encoded.txt";  // Saving to encoded_files folder
-
-    // Open the input file
-    FILE* input = fopen(input_file, "rb");
-    if (!input) {
-        cout << "Error opening input file!" << endl;
-        return -1;
-    }
-
-    // Build the frequency table and Huffman tree
-    unordered_map<char, int> freq_table = build_frequency_table(input);
-    Tree_node* root = build_huffman_tree(freq_table);
-
-    // Build the encoding map
-    unordered_map<char, string> encoding_map;
-    build_encoding_map(root, "", encoding_map);
-
-    // Reopen the input file to reset the file pointer
-    fclose(input);
-    input = fopen(input_file, "rb");
-
-    // Create the output file in the encoded_files folder
-    FILE* output = fopen(output_file, "wb");
-    if (!output) {
-        cout << "Error opening output file!" << endl;
-        return -1;
-    }
-
-    // Perform the encoding
-    encode(input, output, encoding_map);
-
-    // Close the files
-    fclose(input);
-    fclose(output);
-
-    cout << "Encoding completed successfully!" << endl;
-
-    return 0;
+	// we will first store 3 unsigned int to our header, 
+    // which are the length of file,length of header and the length of characters
+	const char* file_name = "test.txt";
+    	FILE* input = fopen(file_name, "rb");
+	int result = open_file(input);
+	if (result == -1){
+		return 0;
+	}
+	int file_len = count_length(input);
+    	if (file_len == -1){
+        	return 0;
+    	} 
+	unordered_map<char, int> frequency_table = build_frequency_table(input);
+	priority_queue<minheapnode*, vector<minheapnode*>, compare> huffman_heap = build_huffman_heap(frequency_table);
+	unordered_map<char, string> encoding_map;
+	encoding_map = build_encoding_map(huffman_heap.top(), "", encoding_map);
+    	int map_length = encoding_map.size();
+	string compressed_tree = compress_huffman_tree("", huffman_heap.top());
+	compressed_tree += '0';
+	const char* output_file = "test_output_encoded.txt";
+	FILE* output = fopen(output_file, "wb");
+	fwrite(&file_len, sizeof(unsigned int), 1, output);
+	unsigned int header_len = get_header_length(0, huffman_heap.top());
+	header_len = 10 * map_length;
+	if (header_len % 8 != 0) {
+		header_len = header_len / 8 + 1;
+	}
+	else {
+		header_len = header_len / 8;
+	}
+	cout << "the header has total " << header_len << " bytes" << endl;
+	fwrite(&header_len, sizeof(unsigned int), 1, output);
+    	fwrite(&map_length, sizeof(unsigned int), 1, output);
+	write_header(output,compressed_tree);
+	encoding(input, output, encoding_map);
+	fclose(input);
+	fclose(output);
+	return 0;
 }
